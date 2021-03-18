6cad9e1e59bd37f994bab49500d77be23bd2ddf9
Mohamed A. Negm <mr.negm90@gmail.com>
2021-03-17 21:52:54 -0700

    users linked to posts

--- config/puma.rb
+++ config/puma.rb
@@ -57,3 +57,8 @@ on_worker_boot do
   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
   ActiveRecord::Base.establish_connection
 end
+
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+threads threads_count, threads_count
